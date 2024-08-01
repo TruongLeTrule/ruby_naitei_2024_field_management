25ff9d63f25acc39f606424e2af8a932e853dee8
21020116NguyenQuangAnh <21020116@vnu.edu.vn>
2024-08-01 08:21:37 +0700

    Feature: Update Profile

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,6 +38,10 @@ module SessionsHelper
     cookies.delete :remember_token
   end
 
+  def current_user? user
+    user && user == current_user
+  end
+
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
