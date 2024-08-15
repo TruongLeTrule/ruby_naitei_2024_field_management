e790f624e256b2951e794298f0269753ae7f5395
TruongLeTrule <21522732@gm.uit.edu.vn>
2024-08-15 16:00:12 +0700

    feat: lock account after 5 failed login attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable,
+         :confirmable, :lockable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :vouchers, dependent: :destroy
