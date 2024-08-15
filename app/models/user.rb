bc27850ca438a30217624012bcae7ba55fc68674
TruongLeTrule <21522732@gm.uit.edu.vn>
2024-08-15 16:00:12 +0700

    feat: send confirm mail using Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :vouchers, dependent: :destroy
